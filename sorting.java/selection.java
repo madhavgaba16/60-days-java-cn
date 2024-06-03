b13d88be1f3d4f5b88899ee3500c305970c861e3
madhavgaba16 <madhavgaba555@gmail.com>
2024-06-03 16:36:26 +0530

    correct the mistake

--- sorting.java/selection.java
+++ sorting.java/selection.java
@@ -1,7 +1,7 @@
 package sorting.java;
 
 public class selection {
-    public static void Main(Strings []args){
+    public static void main(String[] args){
         System.out.println("hello");
     }
 }
