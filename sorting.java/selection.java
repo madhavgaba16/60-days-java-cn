7218c7060e2088d4a57a7effa12cd485c680d1a3
madhavgaba16 <madhavgaba555@gmail.com>
2024-06-03 16:32:22 +0530

    hello

--- sorting.java/selection.java
+++ sorting.java/selection.java
@@ -1,7 +1,7 @@
 package sorting.java;
 
 public class selection {
-    public static void main(String[] args) {
+    public static void Main(Strings []args){
         System.out.println("hello");
     }
 }
