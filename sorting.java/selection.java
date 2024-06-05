dddc8e5ba223865d67fee999b014d040de967f6f
madhavgaba16 <madhavgaba555@gmail.com>
2024-06-05 12:09:47 +0530

    selection sort

--- sorting.java/selection.java
+++
@@ -1,7 +0,0 @@
-package sorting.java;
-
-public class selection {
-    public static void main(String[] args){
-        System.out.println("hello");
-    }
-}
--- sorting/selection.java
+++ sorting/selection.java
@@ -1,7 +1,22 @@
 
 
 public class selection {
-    public static void main(String[] args){
-        System.out.println("hello");
-    }
+    public static void selection (int [] arr){
+        for (int i=0 ;i<arr.length-1;i++){
+            int min = arr[i];
+            int minindex = i;
+            for(int j =i+1;j<arr.length;j++){
+                if(arr[j]<min){
+                   min = arr[j];
+  
+                    minindex = j;
+                     }
+  
+            }
+            int temp = arr[i];
+  
+            arr[i]=arr[minindex];
+            arr[minindex]=temp;
+          }
+          }
 }
