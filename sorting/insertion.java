c09014233da7cdf3af804901e5a5ee681e60b389
madhavgaba16 <mgaba_be23@thapar.edu>
2024-06-05 18:25:25 +0530

    insertion sort

--- sorting/insertion.java
+++ sorting/insertion.java
@@ -1,3 +1,18 @@
 public class insertion {
+   
+        public static void insertionSort(int[] arr, int size) {
+              for (int i= 1;i<size ;i++){
+                  int j =i -1 ;
+                 int temp = arr[i];
+    while (j >= 0 && arr[j] > temp) {
+                    arr[j + 1] = arr[j];
+                    j--;
+                }
+                // Place temp in its correct position
+                arr[j + 1] = temp;
+              }
+            //Your code goes here
+        }
+    }
     
-}
+
