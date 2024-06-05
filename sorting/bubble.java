681d8298c5df8a2ebdf5addc17693b303cb31694
madhavgaba16 <madhavgaba555@gmail.com>
2024-06-05 12:53:59 +0530

    bubble sort

---
+++ sorting/bubble.java
@@ -0,0 +1,18 @@
+public class bubble{
+    public static void bubble(int []arr){
+        for(int i =arr.length-1;i>0;i--){
+            for(int j =0 ;j<i;j++){
+
+                if(arr[j]>arr[j+1]){
+                    int temp = arr[j];
+                    arr[j]= arr[j+1];
+                    arr[j+1]=temp;
+                     }
+        }
+    
+        //Your code goes here
+    }
+    
+        
+    }
+}
