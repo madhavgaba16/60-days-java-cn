b1d37a5e316976d58b4219f7a41ffd11bc5d5c99
madhavgaba16 <mgaba_be23@thapar.edu>
2024-06-06 14:31:24 +0530

    merge sort

---
+++ sorting/merge.java
@@ -0,0 +1,33 @@
+public class merge {
+    public static int[] mergesort(int arr1[], int arr2[]) {
+        int[] arr3 = new int[arr1.length+arr2.length];
+        int i =0 ;
+        int j=0 ;
+         int k =0 ;
+         while(i<arr1.length&&j<arr2.length){
+             if(arr1[i]<arr2[j]){
+                 arr3[k]=arr1[i];
+                 k++;
+                 i++;
+                  }
+                 else{
+                 arr3[k]=arr2[j];
+                 k++;
+                 j++;
+                  }
+         }
+         while(i<arr1.length){
+             arr3[k]=arr1[i];
+             k++;
+             i++;
+
+         }
+         while(j<arr2.length){
+             arr3[k]=arr2[j];
+             k++;
+             j++;
+
+         }
+         return arr3;
+         
+}
