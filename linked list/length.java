b00fe9437afeae780ea6cc25c7b4f310310a82e6
madhavgaba16 <mgaba_be23@thapar.edu>
2024-06-12 23:37:56 +0530

    length of a array list

---
+++ linked list/length.java
@@ -0,0 +1,23 @@
+
+
+public class length {
+    
+public class Solution {
+    public static int length(Node head){
+		/* Your class should be named Solution
+		 * Don't write main().
+		 * Don't read input, it is passed as function argument.
+		 * Return output and don't print it.
+	 	 * Taking input and printing output is handled automatically.
+		*/
+		int counter=0;
+        while(head!=null)
+        {head=head.next;
+        counter++;}
+        
+        return counter;
+	}
+    
+}
+    
+}
