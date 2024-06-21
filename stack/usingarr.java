4a65198d9f3d7fff53941753d287e466152e2baf
madhavgaba16 <mgaba_be23@thapar.edu>
2024-06-21 17:52:46 +0530

    building satck data structure

---
+++ stack/usingarr.java
@@ -0,0 +1,34 @@
+package stack;
+
+   public class usingarr {
+   private  int data[];
+    private int top ;
+     public usingarr(){
+        data = new int[10];
+        top = -1;
+     }
+      public usingarr(int capacity){
+        data = new int [capacity];
+        top = -1;
+
+      }
+      public boolean isEmpty(){
+        return(top==-1);
+      }
+      public int size(){
+        return top+1;
+      }
+      public int top(){
+        return data[top];//need to throw exception if array is empty so we cann check  if size is 0 throw exception; 
+      }
+      public void  push(int element ){
+           top++;
+           data[top]= element;// here we need to throw exception if size if completely filled and no more element can be added;
+      }
+      public int pop(){
+        int temp = data[top];
+         top--;
+         return temp;// also need to throw exception when if no element in present or sixe in zero ;
+      }
+      
+   }
