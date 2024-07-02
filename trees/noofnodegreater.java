c18c7051a38d45d3acefa8b32da975210f4a88f2
madhavgaba16 <mgaba_be23@thapar.edu>
2024-07-02 18:09:54 +0530

    number of nodes greater than the given value

---
+++ trees/noofnodegreater.java
@@ -0,0 +1,24 @@
+package trees;
+
+public class noofnodegreater {
+    public static int numNodeGreater(TreeNode<Integer> root, int x) {
+        if (root == null)
+            return 0;
+    
+        int count = 0;
+        
+        // Check if current node's data is greater than x
+        if (root.data > x) {
+            count++;
+        }
+    
+        // Recursively count nodes greater than x in children
+        for (int i = 0; i < root.children.size(); i++) {
+            count += numNodeGreater(root.children.get(i), x);
+        }
+    
+        return count;
+    }
+    
+    
+}
