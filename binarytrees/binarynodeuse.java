f544bad4f103ad10c42174424f487f42b5868832
madhavgaba16 <mgaba_be23@thapar.edu>
2024-08-03 15:19:00 +0530

    print recursively

--- binarytrees/binarynodeuse.java
+++ binarytrees/binarynodeuse.java
@@ -1,14 +1,52 @@
 
     package binarytrees;
 
-public class binarynodeuse {
-     public static void main(String[] args) {
-      binarynode<Integer> root = new binarynode<Integer>(10);
-      binarynode<Integer> left = new binarynode<Integer>(11);
-      binarynode<Integer>right = new binarynode<Integer>(112);
-      root.left = left ;
-      root.right = right;
+import java.util.Scanner;
 
+public class binarynodeuse {
+    public static void print(binarynode<Integer> root){
+        if(root==null ){
+            return ;
+        }
+        String tobeprinted= root.data +" ";
+        if(root.left!= null){
+            tobeprinted = tobeprinted+"L:"+ root.left.data;
+
+        }
+        if(root.right!= null){
+            tobeprinted = tobeprinted+"r:"+ root.right.data;
+
+        }
+        System.out.println(tobeprinted);
+        print(root.left);
+        print(root.right);
+
+
+        
+
+
+
+    }
+    public static  binarynode<Integer> takeInput (Scanner s) {
+         int rootdata;
+         System.out.println("enter root data");
+         rootdata= s.nextInt();
+         if(rootdata==-1){
+            return null 
+;
+
+         }
+         binarynode<Integer> root = new binarynode<Integer>(rootdata);
+         root.left =takeInput(s);
+         root.right= takeInput(s);
+         return root;
+
+ 
+    }
+     public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+      binarynode<Integer> root = takeInput(s);
+      print(root);
      }
     
 }
--- binarytrees/tempCodeRunnerFile.java
+++
@@ -1,1 +0,0 @@
-      root.right = right;
