b767527eb300bd57be0aeadcd95af0a075e310a5
madhavgaba16 <mgaba_be23@thapar.edu>
2024-08-03 14:44:17 +0530

    binary trees

--- binarytrees/binarynodeuse.java
+++ binarytrees/binarynodeuse.java
@@ -1,13 +1,17 @@
-package binarytrees;
+
+    package binarytrees;
 
 public class binarynodeuse {
      public static void main(String[] args) {
       binarynode<Integer> root = new binarynode<Integer>(10);
       binarynode<Integer> left = new binarynode<Integer>(11);
-      binarynode<Integer> right = new binarynode<Integer>(112);
-      root.left = left;
+      binarynode<Integer>right = new binarynode<Integer>(112);
+      root.left = left ;
       root.right = right;
 
      }
     
 }
+
+    
+
