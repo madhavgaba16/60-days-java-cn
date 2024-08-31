9edb1b5bf361b9b6839036d36db09d6c27651cfc
madhavgaba16 <mgaba_be23@thapar.edu>
2024-09-01 01:56:52 +0530

    hashmaps

--- haskmaps/firstclass.java
+++ haskmaps/firstclass.java
@@ -2,14 +2,15 @@
 //  import java.util.Scanner;
  import java.util.HashMap;
  public class firstclass {
-   public static void main(){
+   public static void main(String[]args){
    HashMap<String,Integer> map = new HashMap<>();
         map.put("abc",1);
         map.put("def",1);
          if(map.containsKey("abc")){
             System.out.println("has abc ");
          }
-         
+
    }
     
 }
+
